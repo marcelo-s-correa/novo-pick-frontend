5df8e332ea0bdba3e114fe04b91d6d2ad6d68384
marcelo-s-correa <marcelo.s.correa10@gmail.com>
2023-05-05 10:01:22 -0300

    adicionando roteamento no game e sair temporario

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,11 +2,12 @@
   <v-app id="inspire">
     <v-app-bar class="px-3" color="white" flat density="compact">
       <v-tabs centered color="grey-darken-2">
-        <v-tab v-for="link in links" :key="link">
-          {{ link }}
+        <v-tab @click="navToTab(link.to)" v-for="link in links" :key="link">
+          {{ link.text }}
         </v-tab>
       </v-tabs>
       <v-spacer></v-spacer>
+      <strong @click="logout">Sair</strong>
     </v-app-bar>
     <div class="game-container">
       <router-view></router-view>
@@ -19,10 +20,35 @@ export default {
   name: "AppGame",
   data: () => ({
     pageTitle: "Game | Pick ",
-    links: ["Game", "Perfil", "Ranking"],
+    links: [
+      {
+        text: "Game",
+        icon: "mdi-home",
+        to: "/game",
+      },
+      {
+        text: "Perfil",
+        icon: "mdi-home",
+        to: "/game/profile",
+      },
+      {
+        text: "Ranking",
+        icon: "mdi-home",
+        to: "/game/ranking",
+      },
+    ],
   }),
   mounted() {
     document.title = this.pageTitle;
+  },
+  methods: {
+    navToTab(to) {
+      this.$router.push(to);
+    },
+    logout() {
+      localStorage.clear();
+      this.$router.push("/login");
+    },
   },
 };
 </script>
